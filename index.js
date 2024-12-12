38e0d845aa3e2b38c99b07771562c4f414c88033
Steamgx <your-bmuraya646@gmail.com>
2024-12-12 08:46:46 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-function addingEventListener() {
+/* const input = document.getElementById('button');
+input.addEventListener('click', function() {
+   return alert('I was clicked!');
+}); */
+/*  const input=document.getElementById('button');
+input.addEventListener("click",function(){
+  return alert("I was clicked!")
+}) */
+  function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener("click", function () {
+        alert("I was clicked!");
+    });
+}
+
+// Export the function if necessary for the test environment
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { addingEventListener };
 }
